f0be7990724c7b89831a63c52a6243365e1e5c80
Yongjia Sun <thomastank0926@gmail.com>
2022-10-21 06:08:09 +0000

    Parameterized Query

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,15 +7,17 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const cohortName = [process.argv[2] || 'JUL02'];
+
 pool.query(`
 SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
 FROM assistance_requests
 JOIN teachers ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`)
+`, cohortName)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort}: ${user.teacher}`);
